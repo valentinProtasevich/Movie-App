52e540bcdc7c8f59b549f936f246053f46e40adf
Valentin <veksel_or_vels@mail.ru>
2022-08-29 17:42:00 +0300

    create folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/app/App';
 
+import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
