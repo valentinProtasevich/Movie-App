2694ce9d57655d460fb669a2f7bc782753f09b33
Valentin <veksel_or_vels@mail.ru>
2022-08-30 10:41:18 +0300

    create appFooter

---
+++ src/components/appFooter/AppFooter.js
@@ -0,0 +1,28 @@
+import './appFooter.scss';
+
+import facebook from '../../resources/img/facebook-icon.svg';
+import instagram from '../../resources/img/instagram-icon.svg';
+import linkedin from '../../resources/img/linkedin-icon.svg';
+
+const AppFooter =() => {
+  return (
+    <div className='footer__flex'>
+      <a href="mailto:info@movieApp.com" target='_blank' rel="noreferrer">info@movieApp.com</a>
+      <a href="tel:+375337353333">+375 33 375 33 33</a>
+      <div className='footer__socialContainer'>
+        <a href="https://www.facebook.com/" className='footer__facebook' target='_blank' rel="noreferrer">
+          <img src={facebook} alt="Facebook icon" />
+        </a>
+        <a href="https://www.instagram.com/" className='footer__instagram' target='_blank' rel="noreferrer">
+          <img src={instagram} alt="Instagram icon" />
+        </a>
+        <a href="https://www.linkedin.com/" className='footer__linkedin' target='_blank' rel="noreferrer">
+          <img src={linkedin} alt="Linkedin icon" />
+        </a>
+      </div>
+      <p>MovieApp 2022 Copyright</p>
+    </div>    
+  )
+}
+
+export default AppFooter;
