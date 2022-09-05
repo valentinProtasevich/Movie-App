ab42606f6b48adce96a8adbbbe2dbdb15e230b1d
Valentin <veksel_or_vels@mail.ru>
2022-09-05 17:30:18 +0300

    update header

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,8 +1,51 @@
 import { NavLink, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import {useAuth} from '../../hooks/useAuth';
+import { removeUser } from '../store/slices/userSlice';
 
 import './appHeader.scss';
 
 const AppHeader = () => {
+  const dispatch = useDispatch();
+
+  const {isAuth} = useAuth();
+
+  const accountButtons = () => {
+    if (isAuth) {
+      return (
+        <>
+          <li><NavLink
+              onClick={() => {
+                dispatch(removeUser());
+                activateMenu();
+              }} 
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+              to='/login'>Выйти</NavLink>
+          </li>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <li><NavLink 
+              onClick={activateMenu}
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+              to='/login'>Вход</NavLink>
+          </li>
+          <li><NavLink 
+              onClick={activateMenu}
+              end 
+              className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+              to='/registration'>Регистрация</NavLink>
+          </li>
+        </>
+      )
+    }
+  };
+
   const activateMenu = () => {
     document.querySelector('.app__navigation').classList.toggle('active');
     document.querySelector('.app__header_burgerBtn').classList.toggle('active');
@@ -26,51 +69,28 @@ const AppHeader = () => {
           <li><NavLink 
                 onClick={activateMenu}
                 end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+                className={({ isActive }) => "" + (isActive ? "app__navigation_selected" : "")}
                 to=''>Главная</NavLink>
           </li>
-          {/* <li><NavLink 
-                onClick={activateMenu}
-                end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
-                to='/films'>Фильмы</NavLink>
-          </li>
-          <li><NavLink 
-                onClick={activateMenu}
-                end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
-                to='/series'>Сериалы</NavLink>
-          </li> */}
           <li><NavLink 
             onClick={activateMenu}
             end
-            className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+            className={({ isActive }) => "" + (isActive ? "app__navigation_selected" : "")}
             to='/movie'>Фильмы</NavLink>
           </li>
           <li><NavLink 
                 onClick={activateMenu}
                 end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+                className={({ isActive }) => "" + (isActive ? "app__navigation_selected" : "")}
                 to='/tv'>Сериалы</NavLink>
           </li>
           <li><NavLink 
                 onClick={activateMenu}
                 end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
+                className={({ isActive }) => "" + (isActive ? "app__navigation_selected" : "")}
                 to='/actors'>Актеры</NavLink>
           </li>
-          <li><NavLink 
-                onClick={activateMenu}
-                end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
-                to='/login'>Вход</NavLink>
-          </li>
-          <li><NavLink 
-                onClick={activateMenu}
-                end
-                className={({ isActive }) => "" + (isActive ? " app__navigation_selected" : "")}
-                to='/registration'>Регистрация</NavLink>
-          </li>
+          {accountButtons()}
         </ul>
       </nav>
     </header>
