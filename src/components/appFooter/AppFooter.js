25d027d176f3f28723f8d0452a0123723d051a7b
Valentin <veksel_or_vels@mail.ru>
2022-09-08 12:40:12 +0300

    fix appFooter

--- src/components/appFooter/AppFooter.js
+++ src/components/appFooter/AppFooter.js
@@ -6,22 +6,24 @@ import linkedin from '../../resources/img/linkedin-icon.svg';
 
 const AppFooter =() => {
   return (
-    <div className='footer__flex'>
-      <a href="mailto:info@movieApp.com" target='_blank' rel="noreferrer">info@movieApp.com</a>
-      <a href="tel:+375337353333">+375 33 375 33 33</a>
-      <div className='footer__socialContainer'>
-        <a href="https://www.facebook.com/" className='footer__facebook' target='_blank' rel="noreferrer">
-          <img src={facebook} alt="Facebook icon" />
-        </a>
-        <a href="https://www.instagram.com/" className='footer__instagram' target='_blank' rel="noreferrer">
-          <img src={instagram} alt="Instagram icon" />
-        </a>
-        <a href="https://www.linkedin.com/" className='footer__linkedin' target='_blank' rel="noreferrer">
-          <img src={linkedin} alt="Linkedin icon" />
-        </a>
+    <footer className='footer__flex'>
+      <div className='footer__container'>
+        <a href="mailto:info@movieApp.com" target='_blank' rel="noreferrer">info@movieApp.com</a>
+        <a href="tel:+375337353333">+375 33 375 33 33</a>
+        <div className='footer__socialContainer'>
+          <a href="https://www.facebook.com/" className='footer__facebook' target='_blank' rel="noreferrer">
+            <img src={facebook} alt="Facebook icon" />
+          </a>
+          <a href="https://www.instagram.com/" className='footer__instagram' target='_blank' rel="noreferrer">
+            <img src={instagram} alt="Instagram icon" />
+          </a>
+          <a href="https://www.linkedin.com/" className='footer__linkedin' target='_blank' rel="noreferrer">
+            <img src={linkedin} alt="Linkedin icon" />
+          </a>
+        </div>
+        <p>MovieApp 2022 Copyright</p>
       </div>
-      <p>MovieApp 2022 Copyright</p>
-    </div>    
+    </footer>    
   )
 }
 
